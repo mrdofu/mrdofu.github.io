421ec0331b904b1cf8320160e0f4a8e011bd6eab
mrdofu <intense_smurf@live.com>
2016-06-02 02:41:14 -0400

    Revert "quick fix for gh not finding the js files"
    This reverts commit 79efb6f5ef08b738a0dde92eeb8de419085917dd.

--- components/slidingPanel/panelController.js
+++
@@ -1,7 +0,0 @@
-angular.module('pageslide-directive').controller('PanelController', ['$scope', function($scope){
-	$scope.checked = false;
-
-	$scope.togglePanel = function(){
-		$scope.checked = !$scope.checked;
-	}
-}]);
